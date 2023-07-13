8e93ccf09927ff39814127791c55351c7ccf221d
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2023-07-13 11:37:34 +0530

    first commit

---
+++ server/src/main/java/com/vishwa/server/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.vishwa.server.controller;
+
+import com.vishwa.server.model.User;
+import com.vishwa.server.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody  User newUser){
+        return  userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+}
