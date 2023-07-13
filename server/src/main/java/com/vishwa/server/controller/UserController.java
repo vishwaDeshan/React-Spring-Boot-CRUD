3ee95299183706d0194e5687271a248bbc4ce22a
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2023-07-13 15:23:06 +0530

    test

--- server/src/main/java/com/vishwa/server/controller/UserController.java
+++ server/src/main/java/com/vishwa/server/controller/UserController.java
@@ -4,7 +4,6 @@ import com.vishwa.server.model.User;
 import com.vishwa.server.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
